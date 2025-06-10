fc77c14dcd8d932ad1fd9da6965137a03638ed64
João Vitorr Freitas Wolf <joaovitorfreitaswolff1@outlook.com>
2025-06-10 22:51:35 +0000

    Movimentação Bispo

--- desafio_xadrez.c
+++ desafio_xadrez.c
@@ -2,12 +2,20 @@
 
 int main(){
 
-    //Movimento da Torre, 5 casas para a direita usando a estrutura for
-    int i;
-    
+    //Movimento da Torre, 5 casas para a direita utlizando a estrutura for
+
     printf("Movimento da Torre:\n");
+    int i;
     for (i = 1; i <= 5; i++) {
         printf("Direita (%d)\n", i);
+    }
+
+    // Movimento do Bispo, 5 casas na diagonal utilizando a estrutura while
+    printf("\nMovimento do Bispo:\n");
+    int j = 1;
+    while (j <= 5) {
+        printf("Cima, Direita (%d)\n", j);
+        j++;
     }
 
     return 0;
